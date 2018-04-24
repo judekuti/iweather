9bf4a772781be2b501856eae90c82ec80360e56b
Jude Kuti <judekuti@gmail.com>
2018-04-24 09:51:02 -0500

    Get api key and url as variables in class

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the WeatherProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WeatherProvider {
+  apiKey = '5e1f89fe4ed775b4';
+  url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
 
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
   }
+
+getWeather(city, state){
+  return this.http.get(this.url+'/'+state+'/'+city+'.json')
+}
 
 }
